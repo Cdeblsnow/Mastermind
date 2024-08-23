d2a03788c53d721458c0a94005d6039ab8a82b52
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-23 00:10:12 -0400

    Add new methods and other minor changes for test

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,10 @@
 class Game
-  @@feddback = []
+  @@fedback = []
+
+  def self.new_round
+    @@fedback = []
+  end
+
   def self.human_answer(ans)
     @human_ans = ans
   end
@@ -8,17 +13,37 @@ class Game
     @computer_code = code
   end
 
-  def black_fedback
-    4.times do |i|
-      @@feddback.puhs("B".colorize(:black)) if @human_ans[i] == @comp_code[i]
+  def self.show
+    puts "#{@human_ans} human "
+    puts @computer_code
+  end
+
+  # black if the colors are in the right place, white if the color is in the code but in the wrong place
+  def self.black_fedback
+    @human_ans.each_with_index do |arr1, i1|
+      @computer_code.each_with_index do |arr2, i2|
+        @@fedback.push("B".colorize(:black)) if arr1 == arr2 && i1 == i2
+      end
     end
   end
 
-  def white_fedback
+  def self.white_fedback
     @human_ans.each_with_index do |arr1, i1|
-      @comp_code.each_with_index do |arr2, i2|
-        @@feddback.puhs("W".colorize(:white)) if arr1 == arr2 && i1 != i2
+      @computer_code.each_with_index do |arr2, i2|
+        @@fedback.push("W".colorize(:white)) if arr1 == arr2 && i1 != i2
       end
     end
+  end
+
+  def self.show_fedback
+    Game.black_fedback
+    Game.white_fedback
+    puts @@fedback.join(" ")
+  end
+
+  def self.win?
+    return unless @computer_code == @human_ans
+
+    true
   end
 end
