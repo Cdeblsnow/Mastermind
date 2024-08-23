dfc7e8121e6dc67b0d543ed544a980e68d2be0f6
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-23 00:09:11 -0400

    Correct the array in code_generator

--- lib/machine.rb
+++ lib/machine.rb
@@ -2,18 +2,19 @@ require "colorize"
 require_relative "game"
 class Machine
   @@colors = ["R".colorize(:red), "B".colorize(:blue), "Y".colorize(:yellow),
-              "G".colorize(:green), "P".colorize(:magenta), "G".colorize(:gray)]
+              "G".colorize(:green), "P".colorize(:magenta), "H".colorize(:gray)]
   def self.show_colors
     puts @@colors.join(" ")
   end
 
   def self.code_generator
-    color_code = @@colors.shuffle
-    @code = color_code
+    color_code = %w[R B Y G P H].shuffle
+    @code = color_code.slice(0, 4)
+
     Game.comp_code(@code)
   end
 
-  def show_code
-    @code
+  def self.show_code
+    puts @code
   end
 end
