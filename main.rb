5983306612579d1dbdddfe522e0231355b12b729
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-23 00:08:12 -0400

    Work on the game skeleton

--- main.rb
+++ main.rb
@@ -3,9 +3,19 @@ require_relative "lib/game"
 require_relative "lib/machine"
 require_relative "lib/player"
 
+Machine.code_generator
+Machine.show_code
 12.times do |i|
   puts "You have #{12 - i} turns to guess the code"
   Machine.show_colors
-  answer = gets.chomp
+  answer = gets.chomp.upcase
   Player.get_answer(answer)
+  Game.show
+  if Game.win? == true
+    puts "COngratulations!, you have guessed the code"
+    break
+  else
+    Game.show_fedback
+    Game.new_round
+  end
 end
